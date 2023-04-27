faa05505423a62f582dd9ede41a03aac761cf41d
Nelermont <nelermont@gamil.com>
2023-04-27 23:01:33 +0300

    Добавлен сетевой клиент

---
+++ Gists/ViewModel/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Gists
+//
+//  Created by Дмитрий Подольский on 27.04.2023.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
---
+++ Gists/ViewModel/NetworkError.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  Gists
+//
+//  Created by Дмитрий Подольский on 27.04.2023.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case requestFailed(Error)
+    case decodingFailed(Error)
+}
